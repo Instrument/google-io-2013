bed53a5465c64f28dbf2104c168209a5bda2a162
Thomas Reynolds <me@tdreyno.com>
2012-12-21 13:14:49 -0800

    localstorage css too

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,30 +1,42 @@
-var inIFrame = window.location.href.indexOf('modes') > -1;
-var prefix = inIFrame ? '../' : '';
+(function(window, basket, require) {
+	var inIFrame = window.location.href.indexOf('modes') > -1;
+	var prefix = inIFrame ? '../' : '';
 
-window['basket']['require'](
-	{ 'url': prefix + 'js/vendor/jquery-1.6.4.js', 'key': 'jquery' },
-	{ 'url': prefix + 'js/vendor/paper.js', 'key': 'paper' },
-	{ 'url': prefix + 'js/vendor/physics.js', 'key': 'physics' },
-	{ 'url': prefix + 'js/vendor/tuna.js', 'key': 'tuna' },
-	{ 'url': prefix + 'js/vendor/hammer.js', 'key': 'hammer' }
-)['then'](function() {
-	var level2 = [
-		{ 'url': prefix + 'js/vendor/jquery.hammer.js', 'key': 'jquery.hammer' },
-		{ 'url': prefix + 'js/vendor/jquery.specialevent.hammer.js', 'key': 'jquery.specialevent.hammer' }
-	];
-
-	if (inIFrame) {
-		level2.push({ 'url': prefix + 'js/mode.min.js', 'key': 'mode' });
-	}
+	var injectStyle = function(text) {
+		var styletag = document.createElement('style');
+		styletag.innerHTML = text[0];
 	
-	window['basket']['require'].apply(window['basket'], level2)['then'](function() {
+		var head = document.head || document.getElementsByTagName('head')[0];
+		head.appendChild(styletag);
+	};
+
+	require(
+		{ 'url': prefix + 'js/vendor/jquery-1.6.4.js', 'key': 'jquery' },
+		{ 'url': prefix + 'js/vendor/paper.js', 'key': 'paper' },
+		{ 'url': prefix + 'js/vendor/physics.js', 'key': 'physics' },
+		{ 'url': prefix + 'js/vendor/tuna.js', 'key': 'tuna' },
+		{ 'url': prefix + 'js/vendor/hammer.js', 'key': 'hammer' }
+	)['then'](function() {
+		var level2 = [
+			{ 'url': prefix + 'js/vendor/jquery.hammer.js', 'key': 'jquery.hammer' },
+			{ 'url': prefix + 'js/vendor/jquery.specialevent.hammer.js', 'key': 'jquery.specialevent.hammer' }
+		];
+
 		if (inIFrame) {
-			var parts = window.location.href.split('/');
-			var page = parts[parts.length-1];
-			var scriptName = page.replace('.html', '.js');
-			window['basket']['require']({ 'url': prefix + 'js/modes/' + scriptName });
-		} else {
-			window['basket']['require']({ 'url': prefix + 'js/app.min.js', 'key': 'app' });
+			level2.push({ 'url': prefix + 'js/mode.min.js', 'key': 'mode' });
 		}
+	
+		require.apply(basket, level2)['then'](function() {
+			if (inIFrame) {
+				var parts = window.location.href.split('/');
+				var page = parts[parts.length-1];
+				var scriptName = page.replace('.html', '.js');
+				require({ 'url': prefix + 'js/modes/' + scriptName });
+				require({ 'url': prefix + 'css/mode.css', 'key': 'modecss', 'execute': false })['then'](injectStyle);
+			} else {
+				require({ 'url': prefix + 'js/app.min.js', 'key': 'appjs' });
+				require({ 'url': prefix + 'css/app.css', 'key': 'appcss', 'execute': false })['then'](injectStyle);
+			}
+		});
 	});
-});
+})(window, window['basket'], window['basket']['require']);
