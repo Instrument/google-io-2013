a66ad0d1bf8e0ee24fe7b191c1575620436282c5
Brian Hefter <bg.hefter@gmail.com>
2013-01-03 13:03:46 -0800

    More pong code added

--- js/modes/pong.js
+++ js/modes/pong.js
@@ -13,14 +13,21 @@ goog.inherits(ww.mode.PongMode, ww.mode.Core);
 /**
  * Function to initialize the current mode.
  * Creates canvas contexts and sets their size.
+ * Sets initial variables.
  */
 ww.mode.PongMode.prototype.init = function() {
   goog.base(this, 'init');
 
+  /**
+   * Assign each canvas element to a variable.
+   */
   var canvasOne = document.getElementById('canvas-one');
   var canvasTwo = document.getElementById('canvas-two');
   var canvasThree = document.getElementById('canvas-three');
 
+  /**
+   * Assign each canvas context to a variable if the elements exist.
+   */
   if(canvasOne.getContext) {
     var ctxOne = canvasOne.getContext('2d');
   }
@@ -33,6 +40,9 @@ ww.mode.PongMode.prototype.init = function() {
     var ctxThree = canvasThree.getContext('2d');
   }
 
+  /**
+   * Set each canvas element to be the size of the viewport.
+   */
   $('canvasOne').attr('width', window.innerWidth);
   $('canvasOne').attr('height', window.innerHeight);
 
@@ -41,13 +51,39 @@ ww.mode.PongMode.prototype.init = function() {
 
   $('canvasThree').attr('width', window.innerWidth);
   $('canvasThree').attr('height', window.innerHeight);
+
+  /**
+   * Gets the center of the screen.
+   */
+  var screenCenterX = window.innerWidth / 2;
+  var screenCenterY = window.innerHeight / 2;
+
+  /**
+   * Sets the mouse position to start at the screen center.
+   */
+  var mouseX = screenCenterX;
+  var mouseY = screenCenterY;
 };
 
 ww.mode.PongMode.prototype['onclickBlah'] = function() {
   this.playSound('/sounds/cat/cat-1.mp3');
 };
 
+ww.mode.PongMode.drawI = function() {
+  ctxOne.beginPath();
+
+  ctxOne.moveTo(mouseX, mouseY);
+
+  ctxOne.fill();
+}
+
 ww.mode.PongMode.prototype.draw = function() {
   goog.base(this, 'draw');
-  console.log('test');
+
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+  });
+
+  ww.mode.PongMode.drawI();
 };
