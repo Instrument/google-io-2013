3407a64c6a1a0fab05574473b2138dcd21b319db
Stefanie Hatcher <stefhatcher@gmail.com>
2013-01-03 16:27:44 -0800

    little swing action for i cat and growing for o cat.

--- js/modes/cat.js
+++ js/modes/cat.js
@@ -9,10 +9,72 @@ ww.mode.CatMode = function() {
 };
 goog.inherits(ww.mode.CatMode, ww.mode.Core);
 
+ww.mode.CatMode.setRotate = function(elm, deg) {
+  $(elm).css({
+    '-webkit-transform': 'rotate(' + deg + 'deg)',
+    '-moz-transform': 'rotate(' + deg + 'deg)',
+    '-ms-transform': 'rotate(' + deg + 'deg)',
+    'transform': 'rotate(' + deg + 'deg)'
+  });
+};
+
+ww.mode.CatMode.setScale = function(elm, scale) {
+  $(elm).css({
+    '-webkit-transform': 'scale(' + scale + ')',
+    '-moz-transform': 'scale(' + scale + ')',
+    '-ms-transform': 'scale(' + scale + ')',
+    'transform': 'scale(' + scale + ')'
+  });
+};
+
 ww.mode.CatMode.prototype.activateI = function() {
   this.playSound('cat-1.mp3');
+
+  $('#letter-i').animate({ rotate: -20 },
+    {
+      duration: 100,
+      easing: 'easeInOutBounce',
+      step: function(now, fx) {
+        ww.mode.CatMode.setRotate(this, now);
+      }
+    }
+  ).animate({ rotate: 20 },
+    {
+      duration: 200,
+      easing: 'easeInOutBounce',
+      step: function(now, fx) {
+        ww.mode.CatMode.setRotate(this, now);
+      }
+    }
+  ).animate({ rotate: 0 },
+    {
+      duration: 100,
+      easing: 'easeInOutBounce',
+      step: function(now, fx) {
+        ww.mode.CatMode.setRotate(this, now);
+      }
+    }
+  );
 };
 
 ww.mode.CatMode.prototype.activateO = function() {
   this.playSound('cat-2.mp3');
+
+  $('#letter-o').animate({ scale: 1.25 },
+    {
+      duration: 200,
+      easing: 'easeInOutBounce',
+      step: function(now, fx) {
+        ww.mode.CatMode.setScale(this, now);
+      }
+    }
+  ).animate({ scale: 1 },
+    {
+      duration: 100,
+      easing: 'easeInOutBounce',
+      step: function(now, fx) {
+        ww.mode.CatMode.setScale(this, now);
+      }
+    }
+  );
 };
