2f6bb8c6584eb8e3241ba75abaddc1150d79e1b8
Stefanie Hatcher <stefhatcher@gmail.com>
2013-01-05 18:02:02 -0800

    moved pinata to center of screen. added some responsiveness to pinata. pinata now has bouncy balls come out of it. added a max number of hits. need game over state. need to clear paper objects on reset.

--- js/modes/pinata.js
+++ js/modes/pinata.js
@@ -1,119 +1,193 @@
 goog.require('ww.mode.Core');
 goog.provide('ww.mode.PinataMode');
 
-/**
- * @constructor
- */
+
 ww.mode.PinataMode = function() {
   goog.base(this, 'pinata', false, true, false);
 };
 goog.inherits(ww.mode.PinataMode, ww.mode.Core);
 
-ww.mode.PinataMode.PARTY_COLORS = ['#EB475A', '#E05A91', '#925898',
-                                   '#E96641','#19A281', '#FAD14A'];
 
+/**
+ * @private
+ */
 ww.mode.PinataMode.prototype.init = function() {
   goog.base(this, 'init');
-  
+
+  this.COLORS_ = ['#EB475A', '#E05A91', '#925898',
+                  '#E96641','#19A281', '#FAD14A'];
+
+  this.NUM_COLORS = this.COLORS_.length;
+
   var self = this;
 
-  var evt;
   if (this.hasTouch) {
-    evt = 'tap';
+    this.evt = 'tap';
   } else {
-    evt = 'click';
+    this.evt = 'click';
   }
 
-  // Prep paperjs
-  var can = this.getPaperCanvas_();
-  var ctx = can.getContext('2d');
-  var prefixed = Modernizr['prefixed']('imageSmoothingEnabled');
-  ctx[prefixed] = false;
+  this.screenWidth = window.innerWidth;
+  this.screenHeight = window.innerHeight;
+  this.centerX = this.screenWidth / 2;
+  this.centerY = this.screenHeight / 2;
+  this.scale = this.screenWidth / 30;
 
-  // Prepare canvas, especially for retina
-  // this.canvas = document.getElementById('confetti');
-  // this.ctx = this.canvas.getContext('2d');
-
-  // this.width = $(window).width();
-  // this.height = $(window).height();
-
-  // this.canvas.width = this.width;
-  // this.canvas.height = this.height;
-
-  // var devicePixelRatio = window['devicePixelRatio'] || 1,
-  //     backingStoreRatio = this.ctx.webkitBackingStorePixelRatio ||
-  //                         this.ctx.mozBackingStorePixelRatio ||
-  //                         this.ctx.msBackingStorePixelRatio ||
-  //                         this.ctx.oBackingStorePixelRatio ||
-  //                         this.ctx.backingStorePixelRatio || 1,
-  //     ratio = devicePixelRatio / backingStoreRatio;
-
-  // if (devicePixelRatio !== backingStoreRatio) {
-  //     var oldWidth = this.canvas.width;
-  //     var oldHeight = this.canvas.height;
-
-  //     this.canvas.width = oldWidth * ratio;
-  //     this.canvas.height = oldHeight * ratio;
-
-  //     this.canvas.style.width = oldWidth + 'px';
-  //     this.canvas.style.height = oldHeight + 'px';
-
-  //     this.ctx.scale(ratio, ratio);
-  // }
+  this.getPaperCanvas_();
 
   this.balls = [];
 
-  this.addBall();
-  this.createWalls();
-
   this.pinata = $('#pinata');
   this.pinata.css('opacity', '1');
+  this.hitCount = 0;
+  this.maxHit = 10;
 };
 
-ww.mode.PinataMode.prototype.createWalls = function() {
-  this.topWall_ = new paper['Path']['Rectangle'](
-    new paper['Rectangle'](
-      new paper['Point'](0, 0),
-      new paper['Size'](window.innerWidth, 0)
-    )
-  );
-  this.topWall_['fillColor'] = 'orange';
 
-  this.bottomWall_ = new paper['Path']['Rectangle'](
-    new paper['Rectangle'](
-      new paper['Point'](0, window.innerHeight - 0),
-      new paper['Size'](window.innerWidth, 0)
-    )
-  );
-  this.bottomWall_['fillColor'] = 'orange';
+/**
+ * @private
+ */
+ww.mode.PinataMode.prototype.didFocus = function() {
+  goog.base(this, 'didFocus');
 
-  this.leftWall_ = new paper['Path']['Rectangle'](
-    new paper['Rectangle'](
-      new paper['Point'](-0, 0),
-      new paper['Size'](0, window.innerHeight)
-    )
-  );
-  this.leftWall_['fillColor'] = 'orange';
+  var self = this;
 
-  this.rightWall_ = new paper['Path']['Rectangle'](
-    new paper['Rectangle'](
-      new paper['Point'](window.innerWidth - 0, 0),
-      new paper['Size'](0, window.innerHeight)
-    )
-  );
-  this.rightWall_['fillColor'] = 'orange';
+  self.pinata.bind(self.evt, function() {
+    self.addBalls_();
+  });
 };
 
-// ww.mode.PinataMode.prototype.onFrame = function(delta) {
-//   goog.base(this, 'onFrame', delta);
-// };
 
-ww.mode.PinataMode.prototype.addBall = function() {
-  var ball = new paper['Path']['Circle'](
-    new paper['Point'](100, 100),
-    20
-  );
-  ball['fillColor'] = '#000000';
+/**
+ * @private
+ */
+ww.mode.PinataMode.prototype.didUnfocus = function() {
+  goog.base(this, 'didUnfocus');
 
-  this.balls.push(ball);
+  this.pinata.unbind(this.evt);
+};
+
+
+/**
+ * @private
+ */
+ww.mode.PinataMode.prototype.onResize = function(redraw) {
+  goog.base(this, 'onResize', redraw);
+
+  this.screenWidth = window.innerWidth;
+  this.screenHeight = window.innerHeight;
+  this.centerX = this.screenWidth / 2;
+  this.centerY = this.screenHeight / 2;
+  this.scale = this.screenWidth / 30;
+};
+
+
+/**
+ * @private
+ */
+ww.mode.PinataMode.prototype.onFrame = function(delta) {
+  goog.base(this, 'onFrame', delta);
+
+  var length = this.balls.length,
+      size = paper['view']['size'],
+      ball, pre;
+
+  // update balls to bounce within canvas bounds.
+  for (var i = 0; i < length; i ++) {
+    ball = this.balls[i];
+    ball['vector']['y'] += ball['gravity'];
+    ball['vector']['x'] *= 0.99;
+    
+    pre = this.utilAdd_(ball['point'], ball['vector']);
+    
+    if (pre['x'] < ball['radius'] ||
+        pre['x'] > size['width'] - ball['radius']) {
+
+      ball['vector']['x'] *= -1 * ball['dampen'];
+    }
+
+    if (pre['y'] < ball['radius'] ||
+        pre['y'] > size['height'] - ball['radius']) {
+      
+      if (Math.abs(ball['vector']['x']) < 3) {
+        ball['vector'] = paper['Point']['random']();
+        ball['vector'] = this.utilMultiply_(ball['vector'], [150, 100]);
+        ball['vector'] = this.utilAdd_(ball['vector'], [-75, 20]);
+      }
+
+      ball['vector']['y'] *= ball['bounce'];
+    }
+
+    var ballAndVect = this.utilAdd_(ball['point'], ball['vector']);
+    var max = paper['Point']['max'](ball['radius'], ballAndVect);
+ 
+
+    ball['point'] = paper['Point']['min'](max, size['width'] - ball['radius']);
+  
+    ball['position'] = ball['point'];
+    ball['rotate'](ball['vector']['x'] / 2);
+  }
+};
+
+
+/**
+ * @private
+ */
+ww.mode.PinataMode.prototype.utilAdd_ = function(v1, v2) {
+  var result = {};
+
+  result['x'] = (v1['x'] || v1[0]) + (v2['x'] || v2[0]);
+  result['y'] = (v1['y'] || v1[1]) + (v2['y'] || v2[1]);
+
+  return result;
+};
+
+
+/**
+ * @private
+ */
+ww.mode.PinataMode.prototype.utilMultiply_ = function(v1, v2) {
+  var result = {};
+
+  result['x'] = (v1['x'] || v1[0]) * (v2['x'] || v2[0]);
+  result['y'] = (v1['y'] || v1[1]) * (v2['y'] || v2[1]);
+
+  return result;
+};
+
+
+/**
+ * @private
+ */
+ww.mode.PinataMode.prototype.addBalls_ = function() {
+  var ball, point, radius,
+      size = ~~Random(5, 20);
+
+  if (this.hitCount < this.maxHit) {
+    this.hitCount++;
+
+    this.log('hit #' + this.hitCount + '. adding ' + size + ' more balls.');
+
+    for (var i = 0; i < size; i++) {
+      point = new paper['Point'](this.centerX, this.centerY);
+      radius = this.scale * Math.random() + 10;
+
+      ball = new paper['Path']['Circle'](point, radius);
+      ball['point'] = point;
+      ball['fillColor'] = this.COLORS_[i % this.NUM_COLORS];
+      
+      ball['vector'] = paper['Point']['random']();
+      ball['vector'] = this.utilAdd_(ball['vector'], [-0.5, 0]);
+      ball['vector'] = this.utilMultiply_(ball['vector'], [50, 100])
+
+      ball['dampen'] = 0.4;
+      ball['gravity'] = 3;
+      ball['bounce'] = -0.6;
+      ball['radius'] = radius;
+
+      this.balls.push(ball);
+    }
+  } else {
+    this.log('reached max hits. pinata game over.');
+  }
 };
