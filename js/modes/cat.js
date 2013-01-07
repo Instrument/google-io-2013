13591a273af77bb36f94dd92614bee097a14bdd2
Stefanie Hatcher <stefhatcher@gmail.com>
2013-01-07 10:14:21 -0800

    using tweens instead for cat animations. make long cat long..er and hover cat randomly move

--- js/modes/cat.js
+++ js/modes/cat.js
@@ -5,17 +5,14 @@ goog.provide('ww.mode.CatMode');
  * @constructor
  */
 ww.mode.CatMode = function() {
-  goog.base(this, 'cat', true);
+  goog.base(this, 'cat', true, true);
 };
 goog.inherits(ww.mode.CatMode, ww.mode.Core);
 
-ww.mode.CatMode.setRotate = function(elm, deg) {
-  $(elm).css({
-    '-webkit-transform': 'rotate(' + deg + 'deg)',
-    '-moz-transform': 'rotate(' + deg + 'deg)',
-    '-ms-transform': 'rotate(' + deg + 'deg)',
-    'transform': 'rotate(' + deg + 'deg)'
-  });
+ww.mode.CatMode.prototype.init = function() {
+  goog.base(this, 'init');
+
+  this.prefix = Modernizr['prefixed']('transform');
 };
 
 ww.mode.CatMode.prototype.activateI = function() {
@@ -23,31 +20,24 @@ ww.mode.CatMode.prototype.activateI = function() {
 
   self.playSound('cat-1.mp3');
 
-  self.letterI.animate({ rotate: -20 },
-    {
-      duration: 100,
-      easing: 'easeInOutBounce',
-      step: function(now, fx) {
-        ww.mode.CatMode.setRotate(this, now);
+  var stretchOut = new TWEEN['Tween'](
+    { 'scaleY': 1 })['to'](
+    { 'scaleY': 1.4 }, 200)['onUpdate'](
+      function() {
+        self.letterI[0].style[self.prefix] = 'scaleY(' + this['scaleY'] + ')';
       }
-    }
-  ).animate({ rotate: 20 },
-    {
-      duration: 200,
-      easing: 'easeInOutBounce',
-      step: function(now, fx) {
-        ww.mode.CatMode.setRotate(this, now);
-      }
-    }
-  ).animate({ rotate: 0 },
-    {
-      duration: 100,
-      easing: 'easeInOutBounce',
-      step: function(now, fx) {
-        ww.mode.CatMode.setRotate(this, now);
-      }
-    }
   );
+
+  var stretchBack = new TWEEN['Tween'](
+    { 'scaleY': 1.4 })['to'](
+    { 'scaleY': 1 }, 200)['delay'](200)['onUpdate'](
+      function() {
+        self.letterI[0].style[self.prefix] = 'scaleY(' + this['scaleY'] + ')';
+      }
+  );
+
+  self.addTween(stretchOut);
+  self.addTween(stretchBack);
 };
 
 ww.mode.CatMode.prototype.activateO = function() {
@@ -55,11 +45,26 @@ ww.mode.CatMode.prototype.activateO = function() {
 
   self.playSound('cat-2.mp3');
 
-  self.letterO.addClass('selected');
-  
-  var timer = setTimeout(function() {
-    self.letterO.removeClass('selected');
-    clearTimeout(timer);
-    timer = null;
-  }, 190);
+  var position = [Random(-200, 200), Random(-50, 50)];
+
+  var moveOut = new TWEEN['Tween'](
+    { 'x': 0, 'y': 0 })['to'](
+    { 'x': position[0], 'y': position[1] }, 200)['onUpdate'](
+      function() {
+        var translate = 'translate(' + this['x'] + 'px, ' + this['y'] + 'px)';
+        self.letterO[0].style[self.prefix] = translate;
+      }
+  );
+
+  var moveBack = new TWEEN['Tween'](
+    { 'x': position[0], 'y': position[1] })['to'](
+    { 'x': 0, 'y': 0 }, 200)['delay'](200)['onUpdate'](
+      function() {
+        var translate = 'translate(' + this['x'] + 'px, ' + this['y'] + 'px)';
+        self.letterO[0].style[self.prefix] = translate;
+      }
+  );
+
+  self.addTween(moveOut);
+  self.addTween(moveBack);
 };
