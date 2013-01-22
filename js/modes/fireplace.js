dccb929f79d4d8dec4d19b8b22152e8b22e082d6
Thomas Reynolds <me@tdreyno.com>
2013-01-22 11:28:01 -0800

    fireplace

---
+++ js/modes/fireplace.js
@@ -0,0 +1,43 @@
+goog.require('ww.mode.Core');
+goog.provide('ww.mode.FireplaceMode');
+
+/**
+ * @constructor
+ */
+ww.mode.FireplaceMode = function() {
+  goog.base(this, 'fireplace', true);
+  // this.preloadSound('bomb.wav');
+};
+goog.inherits(ww.mode.FireplaceMode, ww.mode.Core);
+
+ww.mode.FireplaceMode.prototype.init = function() {
+  goog.base(this, 'init');
+
+  $(document.body).css({
+    backgroundImage: 'url(../img/fireplace/fire.gif)',
+    backgroundRepeat: 'no-repeat',
+    'background-size': '100% 100%'
+  });
+
+  // var self = this;
+  // setInterval(function() {
+  //   self.playSound('bomb.wav');
+  // }, 2000);
+};
+
+ww.mode.FireplaceMode.prototype.didFocus = function() {
+  goog.base(this, 'didFocus');
+
+  var self = this;
+  this.playSound('fire.mp3', function(source) {
+    self.sourceSource_ = source;
+  }, true);
+};
+
+ww.mode.FireplaceMode.prototype.didUnfocus = function() {
+  goog.base(this, 'didUnfocus');
+
+  if (this.sourceSource_) {
+    this.sourceSource_.disconnect(0);
+  }
+};
