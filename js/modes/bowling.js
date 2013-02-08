8cbf12e8f3732200c54a697389ec60597a4d91db
Stefanie Hatcher <stefhatcher@gmail.com>
2013-02-08 10:14:20 -0800

    bowling animation and sound adjustments

--- js/modes/bowling.js
+++ js/modes/bowling.js
@@ -8,7 +8,8 @@ goog.provide('ww.mode.BowlingMode');
 ww.mode.BowlingMode = function() {
   goog.base(this, 'bowling', true, true, false);
 
-  this.preloadSound('bowling.m4a');
+  this.preloadSound('strike.m4a');
+  this.preloadSound('whoosh-1.wav');
 };
 goog.inherits(ww.mode.BowlingMode, ww.mode.Core);
 
@@ -39,6 +40,9 @@ ww.mode.BowlingMode.prototype.activateI = function() {
 
   var wiggleRight = new TWEEN.Tween({ 'rotate': -wiggle });
   wiggleRight.to({ 'rotate': wiggle }, 200);
+  wiggleRight.onStart(function() {
+    self.playSound('whoosh-1.wav');
+  });
   wiggleRight.delay(100);
   wiggleRight.onUpdate(function() {
     self.transformElem_(self.$letterI_[0], 'rotate(' + this['rotate'] + 'deg)');
@@ -63,18 +67,20 @@ ww.mode.BowlingMode.prototype.activateI = function() {
 ww.mode.BowlingMode.prototype.activateO = function() {
   goog.base(this, 'activateO');
 
+  this.playSound('strike.m4a');
+
   var self = this;
   var deg = ~~Random(400, 490);
 
   var animateSpin = new TWEEN.Tween({ 'rotate': 0 });
-  animateSpin.to({ 'rotate': 780 }, 500);
+  animateSpin.to({ 'rotate': 360 * 10 }, 3000);
   animateSpin.onUpdate(function() {
     self.transformElem_(self.ballWrapper_, 'rotate(' + this['rotate'] + 'deg)');
   });
 
   var animateMove = new TWEEN.Tween({ 'translateX': 0 });
   animateMove.to({ 'translateX': 180 }, 500);
-  animateMove.delay(200);
+  animateMove.delay(1000);
   animateMove.easing(TWEEN.Easing.Exponential.In);
   animateMove.onUpdate(function() {
     self.transformElem_(self.$letterO_[0],
@@ -83,10 +89,7 @@ ww.mode.BowlingMode.prototype.activateO = function() {
 
   var animatePin = new TWEEN.Tween({ 'rotate': 0, 'scale': 1, 'x': 0 });
   animatePin.to({ 'rotate': -deg, 'scale': 0.65, 'x': -175 }, 300);
-  animatePin.delay(500);
-  animatePin.onStart(function() {
-    self.playSound('bowling.m4a');
-  });
+  animatePin.delay(1500);
   animatePin.onUpdate(function() {
     var transform = 'scale(' + this['scale'] + ') ' +
                     'rotate(' + this['rotate'] + 'deg) ' +
@@ -96,7 +99,7 @@ ww.mode.BowlingMode.prototype.activateO = function() {
 
   var reset = new TWEEN.Tween({ 'opacity': 0 });
   reset.to({ 'opacity': 1 }, 500);
-  reset.delay(2000);
+  reset.delay(3500);
   reset.onStart(function() {
     self.transformElem_(self.ballWrapper_, 'rotate(0deg)');
     self.transformElem_(self.$letterO_[0], 'translate(0px, 0px)');
