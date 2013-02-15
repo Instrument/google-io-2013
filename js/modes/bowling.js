684ed95f6345380721995e7971d175373cf9c7eb
Stefanie Hatcher <stefhatcher@gmail.com>
2013-02-15 10:02:36 -0800

    bowling adjustments

--- js/modes/bowling.js
+++ js/modes/bowling.js
@@ -25,6 +25,8 @@ ww.mode.BowlingMode.prototype.init = function() {
   this.centerX_ = this.$letterO_.attr('cx');
   this.centerY_ = this.$letterO_.attr('cy');
   this.ballCenter_ = [this.centerX_, this.centerY_];
+  this.pinCenter_ = [this.$letterI_.attr('cx'), this.$letterI_.attr('cy')];
+  this.isReset_ = true;
 };
 
 /**
@@ -40,7 +42,8 @@ ww.mode.BowlingMode.prototype.activateI = function() {
   var wiggleLeft = new TWEEN.Tween({ 'rotate': 0 });
   wiggleLeft.to({ 'rotate': -wiggle }, 100);
   wiggleLeft.onUpdate(function() {
-    self.transformElem_(self.$letterI_[0], 'rotate(' + this['rotate'] + 'deg)');
+    self.$letterI_.attr('transform',
+      'rotate(' + this['rotate'] + ', ' + self.pinCenterStr_ + ')');
   });
 
   var wiggleRight = new TWEEN.Tween({ 'rotate': -wiggle });
@@ -50,14 +53,16 @@ ww.mode.BowlingMode.prototype.activateI = function() {
   });
   wiggleRight.delay(100);
   wiggleRight.onUpdate(function() {
-    self.transformElem_(self.$letterI_[0], 'rotate(' + this['rotate'] + 'deg)');
+    self.$letterI_.attr('transform',
+      'rotate(' + this['rotate'] + ', ' + self.pinCenterStr_ + ')');
   });
 
   var wiggleBack = new TWEEN.Tween({ 'rotate': wiggle });
   wiggleBack.to({ 'rotate': 0 }, 100);
   wiggleBack.delay(300);
   wiggleBack.onUpdate(function() {
-    self.transformElem_(self.$letterI_[0], 'rotate(' + this['rotate'] + 'deg)');
+    self.$letterI_.attr('transform',
+      'rotate(' + this['rotate'] + ', ' + self.pinCenterStr_ + ')');
   });
 
   this.addTween(wiggleLeft);
@@ -73,55 +78,63 @@ ww.mode.BowlingMode.prototype.activateO = function() {
   goog.base(this, 'activateO');
 
   this.playSound('strike.m4a');
+  if (this.isReset_) {
+    var self = this;
+    var deg = ~~Random(400, 490);
 
-  var self = this;
-  var deg = ~~Random(400, 490);
+    var animateSpin = new TWEEN.Tween({ 'rotate': 0 });
+    animateSpin.to({ 'rotate': 360 * 5 }, 1500);
+    animateSpin.onUpdate(function() {
+      self.$letterO_.attr('transform', 'rotate(' + (this['rotate'] % 360) + ', ' +
+                              self.centerX_ + ', ' + self.centerY_ + ')');
+    });
 
-  var animateSpin = new TWEEN.Tween({ 'rotate': 0 });
-  animateSpin.to({ 'rotate': 360 * 5 }, 1500);
-  animateSpin.onUpdate(function() {
-    self.$letterO_.attr('transform', 'rotate(' + (this['rotate'] % 360) + ', ' +
-                            self.centerX_ + ', ' + self.centerY_ + ')');
-  });
+    var animateMove = new TWEEN.Tween({ 'translateX': 0 });
+    animateMove.to({ 'translateX': 180 }, 500);
+    animateMove.delay(1000);
+    animateMove.easing(TWEEN.Easing.Exponential.In);
+    animateMove.onUpdate(function() {
+      self.centerX_ = self.centerX_ - this['translateX'];
+      self.transformElem_(self.ballWrapper_[0],
+                            'translateX(-' + this['translateX'] + '%)');
+    });
 
-  var animateMove = new TWEEN.Tween({ 'translateX': 0 });
-  animateMove.to({ 'translateX': 180 }, 500);
-  animateMove.delay(1000);
-  animateMove.easing(TWEEN.Easing.Exponential.In);
-  animateMove.onUpdate(function() {
-    self.centerX_ = self.centerX_ - this['translateX'];
-    self.transformElem_(self.ballWrapper_[0],
-                          'translateX(-' + this['translateX'] + '%)');
-  });
+    var pinX =
+    var animatePin = new TWEEN.Tween({ 'rotate': 0, 'scale': 1, 'x': 0 });
+    animatePin.to({ 'rotate': -deg, 'scale': 0.65, 'x': -175 }, 300);
+    animatePin.delay(1500);
+    animatePin.onUpdate(function() {
+      // var transform = 'scale(' + this['scale'] + ') ' +
+      //                 'rotate(' + this['rotate'] + 'deg) ' +
+      //                 'translateX(' + this['x'] + '%)';
+      // self.transformElem_(self.$letterI_[0], transform);
+    });
+    animatePin.onComplete(function() {
+      self.isReset_ = false;
+    });
 
-  var animatePin = new TWEEN.Tween({ 'rotate': 0, 'scale': 1, 'x': 0 });
-  animatePin.to({ 'rotate': -deg, 'scale': 0.65, 'x': -175 }, 300);
-  animatePin.delay(1500);
-  animatePin.onUpdate(function() {
-    var transform = 'scale(' + this['scale'] + ') ' +
-                    'rotate(' + this['rotate'] + 'deg) ' +
-                    'translateX(' + this['x'] + '%)';
-    self.transformElem_(self.$letterI_[0], transform);
-  });
+    var reset = new TWEEN.Tween({ 'opacity': 0 });
+    reset.to({ 'opacity': 1 }, 500);
+    reset.delay(3000);
+    reset.onStart(function() {
+      self.$letterO_.attr('transform', 'rotate(0, ' + self.ballCenter_[0] + ', ' +
+                          self.ballCenter_[1] + ')');
+      self.centerX_ = self.ballCenter_[0];
+      self.transformElem_(self.ballWrapper_[0], 'translate(0px, 0px)');
+      self.transformElem_(self.$letterI_[0], 'scale(1) rotate(0deg)');
+    });
+    reset.onUpdate(function() {
+      self.$letterO_[0].style['opacity'] = this['opacity'];
+      self.$letterI_[0].style['opacity'] = this['opacity'];
+      self.ballWrapper_[0].style['opacity'] = this['opacity'];
+    });
+    reset.onComplete(function() {
+      self.isReset_ = true;
+    });
 
-  var reset = new TWEEN.Tween({ 'opacity': 0 });
-  reset.to({ 'opacity': 1 }, 500);
-  reset.delay(3500);
-  reset.onStart(function() {
-    self.$letterO_.attr('transform', 'rotate(0, ' + self.ballCenter_[0] + ', ' +
-                        self.ballCenter_[1] + ')');
-    self.centerX_ = self.ballCenter_[0];
-    self.transformElem_(self.ballWrapper_[0], 'translate(0px, 0px)');
-    self.transformElem_(self.$letterI_[0], 'scale(1) rotate(0deg)');
-  });
-  reset.onUpdate(function() {
-    self.$letterO_[0].style['opacity'] = this['opacity'];
-    self.$letterI_[0].style['opacity'] = this['opacity'];
-    self.ballWrapper_[0].style['opacity'] = this['opacity'];
-  });
-
-  this.addTween(animateSpin);
-  this.addTween(animateMove);
-  this.addTween(animatePin);
-  this.addTween(reset);
+    this.addTween(animateSpin);
+    this.addTween(animateMove);
+    this.addTween(animatePin);
+    this.addTween(reset);
+  }
 };
