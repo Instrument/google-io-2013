9ce40595b092b4a010ee4334b689cfb4ca8c1fd0
Stefanie Hatcher <stefhatcher@gmail.com>
2013-02-20 15:08:55 -0800

    omg rocket animation updates. launch wiggles. lands on the moon. moon make a sci-fi-door sound, because, why not.

--- js/modes/rocket.js
+++ js/modes/rocket.js
@@ -9,27 +9,24 @@ goog.provide('ww.mode.RocketMode');
  */
 ww.mode.RocketMode = function(containerElem, assetPrefix) {
   this.preloadSound('rocket-launch.mp3');
+  this.preloadSound('rumble.wav');
+  this.preloadSound('sci-fi-door.wav');
 
   goog.base(this, containerElem, assetPrefix, 'rocket', true, true, false);
 
-  this.moon_ = this.find('#moon-1')[0];
   var i = this.find('.letter-i');
   var o = this.find('.letter-o');
-  this.moonCenter_ = o.attr('cx') + ', ' + o.attr('cy');
-  this.rocketCenter_ = i.attr('cx') + ', ' + i.attr('cy');
+  this.centerO_ = o.attr('cx') + ', ' + o.attr('cy');
+  this.centerI_ = i.attr('cx') + ', ' + i.attr('cy');
+
   this.rocket_ = this.find('.letter-i-wrapper');
 
-  this.moonOver_ = this.find('.letter-o').clone();
-  this.moonOver_.attr('class', 'letter-o-over');
-  this.moonOver_.css('opacity', 0);
-
-  this.find('.letter-i').after(this.moonOver_);
-
-  this.moonOver_.find('#moon-1').attr('id', 'moon-2');
-  this.moonOver_ = this.moonOver_[0];
-
-  this.faces_ = $('[id*=face-]');
-  this.moons_ = $('[id*=moon-]');
+  this.moons_ = $('#moon-1');
+  this.fires_ = $('[id*=fire-]').css('opacity', 0);
+  this.maxFires_ = this.fires_.length;
+  this.currentFire_ = 0;
+  this.hasLanded_ = false;
+  this.isAnimating_ = false;
 };
 goog.inherits(ww.mode.RocketMode, ww.mode.Core);
 
@@ -41,6 +38,74 @@ goog.inherits(ww.mode.RocketMode, ww.mode.Core);
 ww.mode.RocketMode.prototype.activateI = function() {
   goog.base(this, 'activateI');
 
+  if (this.isAnimating_) {
+    return;
+  }
+
+  if (this.currentFire_ < this.maxFires_) {
+    this.animateWiggle_();
+  } else if (!this.hasLanded_) {
+    this.animateLanding_();
+  }
+};
+
+
+/**
+ * Animates rocket launch wiggle.
+ * @private
+ */
+ww.mode.RocketMode.prototype.animateWiggle_ = function() {
+  if (!this.isAnimating_) {
+    var self = this;
+
+    this.playSound('rumble.wav');
+
+    this.fires_[this.currentFire_].style['opacity'] = 1;
+    this.fires_[this.currentFire_ + 1].style['opacity'] = 1;
+    this.fires_[this.currentFire_ + 2].style['opacity'] = 1;
+
+    var wiggle = ~~(Random(2, 2 * this.currentFire_ + 2));
+
+    var wiggleLeft = new TWEEN.Tween({ 'rotate': 0 });
+    wiggleLeft.to({ 'rotate': -wiggle }, 100);
+    wiggleLeft.onUpdate(function() {
+      self.$letterI_.attr('transform',
+        'rotate(' + this['rotate'] + ', ' + self.centerI_ + ')');
+    });
+
+    var wiggleRight = new TWEEN.Tween({ 'rotate': -wiggle });
+    wiggleRight.to({ 'rotate': wiggle }, 200);
+    wiggleRight.delay(100);
+    wiggleRight.onUpdate(function() {
+      self.$letterI_.attr('transform',
+        'rotate(' + this['rotate'] + ', ' + self.centerI_ + ')');
+    });
+
+    var wiggleBack = new TWEEN.Tween({ 'rotate': wiggle });
+    wiggleBack.to({ 'rotate': 0 }, 100);
+    wiggleBack.delay(300);
+    wiggleBack.onUpdate(function() {
+      self.$letterI_.attr('transform',
+        'rotate(' + this['rotate'] + ', ' + self.centerI_ + ')');
+    });
+    wiggleBack.onComplete(function() {
+      self.currentFire_ += 3;
+      self.isAnimating_ = false;
+    });
+
+    this.isAnimating_ = true;
+    this.addTween(wiggleLeft);
+    this.addTween(wiggleRight);
+    this.addTween(wiggleBack);
+  }
+};
+
+
+/**
+ * Animates rocket landing on the moon.
+ * @private
+ */
+ww.mode.RocketMode.prototype.animateLanding_ = function() {
   this.playSound('rocket-launch.mp3');
 
   var self = this,
@@ -48,15 +113,17 @@ ww.mode.RocketMode.prototype.activateI = function() {
       duration = 400,
       moonBounds = this.$letterO_[0].getBoundingClientRect(),
       rocketBounds = this.$letterI_[0].getBoundingClientRect(),
-      distance = ~~(moonBounds['right'] + rocketBounds['width'] +
-                    rocketBounds['height']),
+      distance = ~~(moonBounds['left'] - rocketBounds['height'])
       transform = '', prevTransform = '';
+
+  var flyUp = new TWEEN.Tween({ 'translateY': 0 });
+  flyUp.to({ 'translateY': 20 }, duration)
 
   var rotateIn = new TWEEN.Tween({ 'rotate': 0 });
   rotateIn.to({ 'rotate': 90 }, delay);
   rotateIn.onUpdate(function() {
     self.$letterI_.attr('transform',
-      'rotate(' + this['rotate'] + ', ' + self.rocketCenter_ + ')');
+      'rotate(' + this['rotate'] + ', ' + self.centerI_ + ')');
   });
 
   var orbitOver = new TWEEN.Tween({ 'translateY': 0, 'scale': 1 });
@@ -64,52 +131,50 @@ ww.mode.RocketMode.prototype.activateI = function() {
   orbitOver.delay(delay);
   orbitOver.onUpdate(function() {
     transform = 'scale(' + this['scale'] + ') ' +
-                'translate(0, -' + this['translateY'] + ')';
+                'translate(-20, -' + this['translateY'] + ')';
     self.rocket_.attr('transform', transform);
   });
   orbitOver.onComplete(function() {
-    transform = transform + ' scale(1, -1) ';
     prevTransform = transform;
-    self.rocket_.attr('transform', transform);
   });
 
   delay += duration;
 
-  var orbitBack = new TWEEN.Tween({ 'translateY': 0, 'scale': 1 });
-  orbitBack.to({ 'translateY': distance, 'scale': 4 }, duration);
-  orbitBack.delay(delay);
-  orbitBack.onStart(function() {
-    self.moonOver_.style['opacity'] = 1;
-  });
-  orbitBack.onUpdate(function() {
-    transform = prevTransform + ' translate(0, -' + this['translateY'] + ') ' +
-                'scale(' + this['scale'] + ')';
+  var rotateLand = new TWEEN.Tween({ 'rotate': 0 });
+  rotateLand.to({ 'rotate': -90 }, duration);
+  rotateLand.delay(delay);
+  rotateLand.onUpdate(function() {
+    transform = prevTransform + ' rotate(' +
+                this['rotate'] + ', ' + self.centerI_ + ')';
     self.rocket_.attr('transform', transform);
   });
-  orbitBack.onComplete(function() {
-    prevTransform = transform;
-    self.moonOver_.style['opacity'] = 0;
+  rotateLand.onComplete(function() {
+    self.fires_.css('opacity', 0);
+    self.activateO();
   });
 
   delay += duration;
 
-  var rotateBack = new TWEEN.Tween({ 'rotate': 90 });
-  rotateBack.to({ 'rotate': 0 }, 100);
-  rotateBack.onStart(function() {
-    transform = transform + ' scale(1, -1) ';
-    prevTransform = transform;
-    self.rocket_.attr('transform', transform);
+  var reset = new TWEEN.Tween({ 'opacity': 0 });
+  reset.to({ 'opacity': 1 }, 200);
+  reset.delay(delay + duration + 2000);
+  reset.onStart(function() {
+    self.rocket_.attr('transform', '');
+    self.$letterI_.attr('transform', '');
   });
-  rotateBack.delay(delay);
-  rotateBack.onUpdate(function() {
-    self.$letterI_.attr('transform',
-      'rotate(' + this['rotate'] + ', ' + self.rocketCenter_ + ')');
+  reset.onUpdate(function() {
+    self.$letterI_.css('opacity', this['opacity']);
+  });
+  reset.onComplete(function() {
+    self.currentFire_ = 0;
+    self.hasLanded_ = false;
   });
 
+  this.hasLanded_ = true;
   this.addTween(rotateIn);
   this.addTween(orbitOver);
-  this.addTween(orbitBack);
-  this.addTween(rotateBack);
+  this.addTween(rotateLand);
+  this.addTween(reset);
 };
 
 
@@ -120,8 +185,10 @@ ww.mode.RocketMode.prototype.activateI = function() {
 ww.mode.RocketMode.prototype.activateO = function() {
   goog.base(this, 'activateO');
 
+  this.playSound('sci-fi-door.wav');
+
   var self = this,
-      duration = 2000,
+      duration = 1000,
       deg = this.moons_[0].style[self.prefix_].split('rotate(')[1];
       deg = parseInt(deg) || 0;
 
@@ -129,10 +196,10 @@ ww.mode.RocketMode.prototype.activateO = function() {
   rotateAround.to({ 'rotate': deg + 360 * 2 }, duration);
   rotateAround.onUpdate(function() {
     self.moons_.attr('transform', 'rotate(' +
-                      this['rotate'] + ', ' + self.moonCenter_ + ')');
+                      this['rotate'] + ', ' + self.centerO_ + ')');
   });
   rotateAround.onComplete(function() {
-    self.moons_.attr('transform', 'rotate(0, ' + self.moonCenter_ + ')');
+    self.moons_.attr('transform', 'rotate(0, ' + self.centerO_ + ')');
   });
 
   self.addTween(rotateAround);
